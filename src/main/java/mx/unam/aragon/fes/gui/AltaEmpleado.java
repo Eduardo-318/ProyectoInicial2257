5c74e34c7819d5132767ff9866bd58ae29aed872
Eduardo_Gutierrez <eduardogutierrez318@aragon.unam.mx>
2021-03-03 12:26:09 -0600

    Tarea 2. Codigo Tarea

--- src/main/java/mx/unam/aragon/fes/gui/AltaEmpleado.java
+++ src/main/java/mx/unam/aragon/fes/gui/AltaEmpleado.java
@@ -5,6 +5,7 @@
  */
 package mx.unam.aragon.fes.gui;
 
+import javax.swing.JOptionPane;
 import mx.unam.aragon.fes.Empleado;
 
 /**
@@ -397,7 +398,17 @@ public class AltaEmpleado extends javax.swing.JFrame {
        emp.setApMaterno(this.jTextField3.getText());
        emp.setEdad(Integer.parseInt(jTextField4.getText()));
        emp.setCurp(jTextField5.getText());
+       emp.setDomicilio(new Direccion());
        emp.getDomicilio().setCalle(this.jTextField6.getText());
+       emp.getDomicilio().setNumero(this.jTextField7.getText());
+       emp.getDomicilio().setColonia(this.jTextField8.getText());
+       emp.getDomicilio().setDelegacion(this.jTextField9.getText());
+       emp.getDomicilio().setEstado(this.jTextField10.getText());
+       emp.setNumeroEmpleado(Integer.parseInt(this.jTextField11.getText()));
+       emp.setDepartamento(this.jTextField12.getText());
+       emp.setSueldo(Float.parseFloat(this.jTextField13.getText()));
+       emp.setHorasExtra(Integer.parseInt(this.jTextField14.getText()));
+       int confirmar = JOptionPane.showConfirmDialog(this,"¿Deseas guardar estos datos?");
     }//GEN-LAST:event_jButton5MouseClicked
     private void LimpiarFormulario(){
         this.jTextField1.setText("");
@@ -493,4 +504,10 @@ public class AltaEmpleado extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField8;
     private javax.swing.JTextField jTextField9;
     // End of variables declaration//GEN-END:variables
+
+    private static class Direccion extends mx.unam.aragon.fes.Direccion {
+
+        public Direccion() {
+        }
+    }
 }
